3c464b5b1452b0df3f5956c1233e073076fb7cfc
Aleksey Pirogov <pirogov-aa@mail.ru>
2022-02-21 09:47:57 +0300

    JavaCore homework 2

---
+++ src/Lesson_2_Exeption/ArraySizeExeption.java
@@ -0,0 +1,15 @@
+package Lesson_2_Exeption;
+
+// класс ArrayDataExeption является наследником класса Exception
+public class ArraySizeExeption extends Exception {
+
+    // Перегруженный параметризированный констурктор обрабатывающий объект типа String
+    ArraySizeExeption(String str) {
+        super(str);     // вызов родительского конструктора
+    }
+
+    // Перегруженный параметризированный констурктор обрабатывающий массив объектов типа String
+    ArraySizeExeption(String[][] array) {
+        super("ArraySizeExeption: в функцию передан массив размерности " + array.length + " x " + array[0].length + ", что отличается от 4 x 4 ...");
+    }
+}
